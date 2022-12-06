af980f2c01f8a6b71277386a54661697cef872a0
Donstesh <stephenshifoko@gmailr.com>
2022-12-06 22:56:35 +0300

    added favicon home page

--- template/front/header.php
+++ template/front/header.php
@@ -16,6 +16,7 @@ foreach($site_settings as $row)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="storage/logo/<?php echo $row['site_logo']; ?>" />
+    <link rel="shortcut icon" href="../assets/img/icons/tanzahost.png" />
     <title><?php echo $row['site_name']; ?> | <?php echo $row['site_description']; ?></title>
     <?php } ?>
     <!-- font awesome cdn link  -->
